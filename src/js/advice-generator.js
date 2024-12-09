8fd783ae3ac7258a7722e5bda193c78e611ff921
Lucas Augusto <luccasgaetano19@gmail.com>
2024-12-09 15:27:04 -0300

    Adicionando sessão para tratar erros no JS

--- src/js/advice-generator.js
+++ src/js/advice-generator.js
@@ -3,14 +3,22 @@ const adviceDescription = document.querySelector(".advice-description");
 const btnAdvice = document.querySelector(".btn-advice");
 
 async function genarateRandomAdvice() {
-    const url = await fetch ("https://api.adviceslip.com/advice");
-    const randomAdvice = await url.json();
-    idAdvice.innerHTML = "ADIVICE #" + randomAdvice.slip.id;
-    adviceDescription.innerHTML = randomAdvice.slip.advice;
+    try {
+        const url = await fetch ("https://api.adviceslip.com/advice");
+
+        if(!url.ok){
+            throw new Error("Ocorreu um erro ao tentar buscar as informações da API");
+        }
+
+        const randomAdvice = await url.json();
+        idAdvice.innerText = `ADIVICE #${randomAdvice.slip.id}`;
+        adviceDescription.innerText = `"${randomAdvice.slip.advice}"`;
+
+    } catch (error) {
+        console.error("Erro ao tentar buscar as informações da API", error)
+    }
 }
 
 genarateRandomAdvice();
 
-btnAdvice.addEventListener('click', () => {
-    genarateRandomAdvice();
-})
+btnAdvice.addEventListener('click', () => genarateRandomAdvice());
